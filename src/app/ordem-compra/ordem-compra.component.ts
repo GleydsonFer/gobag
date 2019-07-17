336cd487437cdd60242788c82f8e53de59b3b9b0
lucasDamasceno <lucasdds13@gmail.com>
2019-07-17 12:11:50 -0300

    recebendo código

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,9 +1,11 @@
+import { AngularFireAuth } from '@angular/fire/auth';
+import { UsuarioService } from './../usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
 import { FormGroup, FormControl, Validators } from '@angular/forms'
 
-import  CarrinhoService  from '../carrinho.service'
+import CarrinhoService from '../carrinho.service'
 import { ItemCarrinho } from '../shared/item-carrinho.model'
 import { inspectNativeElement } from '@angular/platform-browser/src/dom/debug/ng_probe';
 
@@ -11,70 +13,25 @@ import { inspectNativeElement } from '@angular/platform-browser/src/dom/debug/ng
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
   styleUrls: ['./ordem-compra.component.css'],
-  providers: [ OrdemCompraService ]
+  providers: [OrdemCompraService]
 })
 export class OrdemCompraComponent implements OnInit {
 
-  public idPedidoCompra: number
-  public itensCarrinho: ItemCarrinho [] = []
+  constructor(
+    private userService: UsuarioService,
+    public afAuth: AngularFireAuth,
+  ) { }
 
-  public formulario: FormGroup = new FormGroup ({
-    'endereco': new FormControl (null,[Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
-    'numero': new FormControl (null,[Validators.required, Validators.minLength(1), Validators.maxLength(20)]),
-    'complemento': new FormControl (null),
-    'formaPagamento': new FormControl (null,[Validators.required])
-  })
-
-  constructor(private ordemCompraService: OrdemCompraService,
-              private carrinhoService: CarrinhoService
-              ) { }
-evert
-  ngOnInit() {
-    this.itensCarrinho = this.carrinhoService.exibirItens()
-    console.log(this.itensCarrinho)
-  }
-
-  public confirmarCompra(): void {
-    if (this.formulario.status === 'INVALID'){
-      
-      this.formulario.get('endereco').markAsTouched()
-      this.formulario.get('numero').markAsTouched()
-      this.formulario.get('complemento').markAsTouched()
-      this.formulario.get('formaPagamento').markAsTouched()
-
-    } else {
-
-      if(this.carrinhoService.exibirItens().length === 0 ){
-        alert('Você não selecionou nenhm item!')
-      }else {
-
-        let pedido: Pedido = new Pedido (
-          this.formulario.value.endereco, 
-          this.formulario.value.numero,
-          this.formulario.value.complemento,
-          this.formulario.value.formaPagamento,
-          this.carrinhoService.exibirItens()
-          )
-
-         
-          this.ordemCompraService.efetivarCompra(pedido)
-          .subscribe((idPedido: number)=> {
-            this.idPedidoCompra = idPedido
-            this.carrinhoService.limparCarrinho()
-            
-          })
-
-      }
-    }
-  }
-
+  ngOnInit(): void {
+    this.afAuth.auth.onAuthStateChanged(user => {
+      console.log(user.email)
+      this.userService.getEnderecoByUsuario(user.email).subscribe((usuario)=>{
+        // console.log(usuario.forEach())
+      })
+    })
   
-  public adicionar (item: ItemCarrinho): void {
-    this.carrinhoService.adicionarQuantidade(item)
   }
-  public diminuir (item: ItemCarrinho): void {
-    this.carrinhoService.diminuirQuantidade(item)
+  public confirmarCompra(): void {
+
   }
-
-
 }
--- src/app/usuario.service.ts
+++ src/app/usuario.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -9,18 +10,15 @@ import { map } from 'rxjs/operators';
 export class UsuarioService {
 
   constructor(private db: AngularFirestore) { }
-
-  // retorna um JSON no formato de Usuário; 
-    // para recuperar os dados basta referenciar o atributo requerido
-    // exemplo: usuario.endereco.logradouro para recuperar a rua do usuário
-    //       ou usuario.nome para recuperar o nome do usuário
-    public getEnderecoByUsuario(key: string){
-      return this.db.collection('usuarios', ref => ref.where(ref.id, '==', key))
-          .snapshotChanges()
-          .pipe(
-              map(changes => {
-                  return changes.map(c => ({ key: c.payload.doc.id, ...c.payload.doc.data() }));
-              })
-          );
+  
+  public getEnderecoByUsuario(key: string) {
+    console.log(key + "isso é ")
+    return this.db.collection("usuarios", ref => ref.where('email', '==', key))
+      .snapshotChanges()
+      .pipe(
+        map(changes => {
+          return changes.map(c => ({ key: c.payload.doc.id, ...c.payload.doc.data() }));
+        })
+      );
   }
 }
