451ef9de77460470f1540a0e65a05be3165c7e95
wesley-feitosa <jwesleydasilva@hotmail.com>
2019-07-17 09:27:56 -0300

    classe produto

--- src/app/shared/produto.model.ts
+++ src/app/shared/produto.model.ts
@@ -1,12 +1,12 @@
 export class Produto {
     public id: number
-    public titulo: string
+    public nome: string
+    public descricao: string
+    public valor: number
     public categoria: Array<string>
     public complemento?: string
     public data: Date
-    public descricao: string
     public destaque: boolean
     public loja: string
-    public valor: number
     public imagens: Array<string>
 }
