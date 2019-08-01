6b1f027a957f649aba0e4e12e2c9ee5d33ab99c2
wesley-feitosa <jwesleydasilva@hotmail.com>
2019-08-01 12:57:04 -0300

    cadastro lojista

--- src/app/cadastro-lojista/cadastro-lojista.component.ts
+++ src/app/cadastro-lojista/cadastro-lojista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-lojista',
@@ -11,5 +12,16 @@ export class CadastroLojistaComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  public formulario: FormGroup = new FormGroup({
+    'nome': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
+    'email': new FormControl(null, [Validators.required, Validators.email]),
+    'cnpj': new FormControl(null, [Validators.required, Validators.minLength(3)]),
+    'senha': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+    'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
+    'numero': new FormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(20)]),
+    'complemento': new FormControl(null)
+    
+  })
 
 }
