b89d7c2465024897bb21177b322f18a6504b9e3b
Wesley Feitosa <jwesleydasilva@gmail.com>
2019-08-01 22:58:38 -0300

    cadastro lojista

--- src/app/shared/lojista.model.ts
+++ src/app/shared/lojista.model.ts
@@ -1,10 +1,19 @@
-export class Lojista{
+export class Lojista {
+    nome: string;
+    sobrenome: string;
+    cargo: string;
+    celular: string;
     email: string;
+    jaPossuiEntregadores: string;
+    possuiQuantasLojas: string;
+    comoChegouAteNos: string;
+    nomeDaLoja: string;
     cnpj: string;
-    endereco: {
-        logradouro: string,
-        numero: number,
-        complemento?: string
-    };
-    telefone: string;
+    cep: string;
+    endereco: string;
+    numero: number;
+    complemento?: string
+    bairro: string;
+    cidade: string;
+    uf: string;
 }
