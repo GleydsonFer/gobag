cb183af12744397fcc3b19b51bc509113f153717
Gleydson Ferreira <gleydsonferreira.ti@gmail.com>
2019-08-04 20:16:47 -0300

    alterações fds

--- src/app/banner-slide/banner-slide.component.ts
+++ src/app/banner-slide/banner-slide.component.ts
@@ -35,10 +35,22 @@ export class BannerSlideComponent implements OnInit {
     }
   ]
 
+  
 
   constructor() { }
 
   ngOnInit() {
   }
 
-}
+//   $(function() {
+//     $(".video").click(function () {
+//       var theModal = $(this).data("target"),
+//       videoSRC = $(this).attr("data-video"),
+//       videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+//       $(theModal + ' iframe').attr('src', videoSRCauto);
+//       $(theModal + ' button.close').click(function () {
+//         $(theModal + ' iframe').attr('src', videoSRC);
+//       });
+//     });
+//   });
+ }
