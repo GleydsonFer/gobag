aa9633d2f57dd98657984a1adc0ecffa07c0d9f6
wesley-feitosa <jwesleydasilva@hotmail.com>
2019-08-22 12:49:23 -0300

    id da transação no pedido e inico da refatoração da coleção de pedidos no banco

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,21 +1,24 @@
-    
+
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Pedido } from './shared/pedido.model';
+import { ListaDePedidos } from './shared/lista-de-pedidos.model';
+import { Subject } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class OrdemCompraService {
 
     constructor(
         private afs: AngularFirestore
-    ) {}
+    ) { }
 
-    public efetivarCompra(pedido: Pedido): Promise<string>{
+    public efetivarCompra(pedido: ListaDePedidos): Promise<string> {
 
-        console.log(pedido);
-        return this.afs.collection('pedidos').add({...pedido}).then(pedido => {
-            return pedido.id;
-        })
-
+        return this.afs.collection('pedidos')
+            .doc(pedido.email_cliente)
+            .collection('lista_pedidos')
+            .add({ ...pedido})
+            .then();
     }
 }
